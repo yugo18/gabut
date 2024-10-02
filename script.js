ba82a04c22989c7347348d4ade409e4fbcae60f4
Yugooooo <ajengyugopangestu10@gmail.com>
2024-10-02 21:25:37 +0700

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate() {
     
     // This will replace the result of any mathematical operation with "I love you"
     if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-        display.value = 'HI WAKHYU NUR AFNI:)';
+        display.value = 'HI COKKK😭:)';
     } else {
         try {
             display.value = eval(expression);
